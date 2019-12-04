1f59916add5cff33ab895a8f01e9c0c4b8bfbf87
viniciush4 <viniciush4@gmail.com>
2019-12-04 18:50:15 -0300

    Texturas em andamento (estão comentadas para não causar segmentation fail)

--- pista.cpp
+++ pista.cpp
@@ -15,6 +15,7 @@ Pista::Pista(){
 	this->largura = 0;
 	this->comprimento = 0;
 	this->angulo_xy = 0;
+	this->textura = LoadTextureRAW("Texturas/pista.bmp");
 }
 
 Pista::Pista(float x1, float y1, float x2, float y2){
@@ -25,6 +26,7 @@ Pista::Pista(float x1, float y1, float x2, float y2){
 	this->largura = 0;
 	this->comprimento = sqrt(pow(y2-y1,2) + pow(x2-x1,2));
 	this->angulo_xy = -radianosParaGraus(atan2((y2-y1), (x2-x1)));
+	// this->textura = LoadTextureRAW("Texturas/pista.bmp");
 }
 
 void Pista::desenhar(){
@@ -35,18 +37,43 @@ void Pista::desenhar(){
     // glEnd();
 
 	// Desenha um plano que representa a pista
+	// glPushMatrix();
+	// 	glPushAttrib(GL_TEXTURE_BIT);
+	// 		glColor3f(1, 1, 1);
+	// 		glNormal3f(0.0, 0.0, 1.0);
+
+	// 		glTranslatef(x1, y1, 1);
+	// 		glRotatef(angulo_xy, 0, 0, 1);
+
+	// 		glBindTexture(GL_TEXTURE_2D, this->textura);
+	// 		glBegin(GL_QUADS);
+	// 			glTexCoord2f(0.0, 0.0); glVertex3f(0, -largura/2, 0.5);
+	// 			glTexCoord2f(0.0, 1.0); glVertex3f(comprimento, -largura/2, 0.5);
+	// 			glTexCoord2f(1.0, 1.0); glVertex3f(comprimento, largura/2, 0.5);
+	// 			glTexCoord2f(1.0, 0.0); glVertex3f(0, largura/2, 0.5);
+	// 		glEnd();
+	// 	glPopAttrib();
+	// glPopMatrix();
+
 	glPushMatrix();
-		glColor3f(1, 1, 0);
-		glTranslatef(x1, y1, 1);
-		glRotatef(angulo_xy, 0, 0, 1);
-		glBegin(GL_QUADS);
-			glColor3f(1.0f, 1.0f, 0.0f);
-			glNormal3f(0.0, 0.0, 1.0);
-			glVertex3f(0, -largura/2, 0.5);
-			glVertex3f(comprimento, -largura/2, 0.5);
-			glVertex3f(comprimento, largura/2, 0.5);
-			glVertex3f(0, largura/2, 0.5);
-		glEnd();
+		// glPushAttrib(GL_TEXTURE_BIT);
+
+			glTranslatef(x1, y1, 1);
+			glRotatef(angulo_xy, 0, 0, 1);
+
+			// glColor3f(1.0f, 1.0f, 1.0f);
+			// glNormal3f(0.0, 0.0, 1.0);
+
+			glBindTexture(GL_TEXTURE_2D, this->textura);
+			glBegin(GL_QUADS);
+				
+				
+				glTexCoord2f(0.0, 0.0); glVertex3f(0, -largura/2, 0.5);
+				glTexCoord2f(0.0, 1.0); glVertex3f(comprimento, -largura/2, 0.5);
+				glTexCoord2f(1.0, 1.0); glVertex3f(comprimento, largura/2, 0.5);
+				glTexCoord2f(1.0, 0.0); glVertex3f(0, largura/2, 0.5);
+			glEnd();
+		// glPopAttrib();
 	glPopMatrix();
 }
 
